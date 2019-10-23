6553c3469edcb7a2d767a14d06fb9c17fa75b7b3
PabloRR100 <pabloruizruiz10@gmail.com>
2019-10-23 20:00:37 +0200

    tomorrow we start on lsa

---
+++ Courses/NLTK/LSA.py
@@ -0,0 +1,10 @@
+
+'''
+LATENT SEMANTIC ANALYSIS
+========================
+
+Analysis of relationships between a set of documents
+and the terms they content by producing a set of concepts
+related to the documents and terms
+'''
+
