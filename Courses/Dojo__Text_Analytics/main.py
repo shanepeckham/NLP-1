d61a59f4fa2e973579b48e57ad8a3fcb5b933bdc
PabloRR100 <pabloruizruiz10@gmail.com>
2019-10-30 09:33:06 +0100

    stratified k-fold

--- Courses/Dojo__Text_Analytics/main.py
+++ Courses/Dojo__Text_Analytics/main.py
@@ -1,21 +1,86 @@
 
 import os
+import re
 import csv
-import nltk
-
-from pathlib import Path
+import heapq
+import numpy as np
+import pandas as pd
+from pprint import pprint
 from os.path import abspath, dirname
 
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('stopwords')
+import nltk
+from nltk.corpus import stopwords
+from nltk import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 workdir = dirname(__file__)
 os.chdir(workdir)
 
-sentences = list()
+# Data
+# ----
+
+mails = list()
 with open('spam.csv') as f:
     for row in csv.reader(f, delimiter=','):
-        sentences.append(row)
+        mails.append(row)
     
-sentences = [nltk.sent_tokenize(sentence) for sentence in sentences]
+df = pd.DataFrame(mails)
+df = df.drop([2,3,4],axis=1).drop(0)
+df.columns = ['Label','Text']
+df['Label'].hist()
+X, y = df['Text'].to_list(), df['Text'].to_list()
+
+
+# Preprocessing
+# -------------
+
+STOPWORDS = stopwords.words('english')
+
+mails = list()
+for i in range(len(X)):
+    mail = X[i]
+    # Lowercase
+    mail = mail.lower()
+    # Non-word characters 
+    mail = re.sub(r'\W', ' ', mail)
+    # Digits
+    mail = re.sub(r'\d', ' ', mail)
+    # Remove single-character words (I, a, ...)
+    mail = re.sub(r'\s+[a-z]\s+', ' ', mail)
+    # Remove single-character as the start of the mail
+    mail = re.sub(r'^[a-z]\s+', ' ', mail)
+    # Remove the extra spaces generated
+    mail = re.sub(r'\s+', ' ', mail)
+    # Rmove spaces
+    mail = re.sub(r'\s+', ' ', mail)
+    # Lemmatize
+    mails.append(lemmatizer.lemmatize(mail))
+
+
+# BAG OF WORDS - Of only the N most frequent words
+# ------------
+
+# 1 - Discover these frequent words
+word_count = dict()
+for mail in mails:
+    for word in nltk.word_tokenize(mail):
+        if word not in STOPWORDS:
+            if word not in word_count.keys():
+                word_count[word] = 1
+            else:
+                word_count[word] += 1
+
+freq_words = heapq.nlargest(30, word_count, key=word_count.get)
+pprint(freq_words)
+
+# 2 - Create the doc-term matrix
+dt_matrix = list()
+for mail in mails:
+    vector = list()
+    for word in freq_words:
+        if word in nltk.word_tokenize(mail):
+            vector.append(1)
+        else:
+            vector.append(0)
+    dt_matrix.append(vector)
+df_matrix = np.asarray(dt_matrix)
--- Courses/Dojo__Text_Analytics/r_translation.py
+++ Courses/Dojo__Text_Analytics/r_translation.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import StratifiedKFold, StratifiedShuffleSplit
+
 import nltk
 from nltk.corpus import stopwords
 from nltk import WordNetLemmatizer
@@ -17,8 +19,9 @@ lemmatizer = WordNetLemmatizer()
 workdir = dirname(__file__)
 os.chdir(workdir)
 
-# Data Exploratory Analysis
-# -------------------------
+
+# Data Exploratory Analysis 
+# --------------------------------------------------
 
 df = pd.read_csv('spam.csv')    
 df.head(2)
@@ -30,7 +33,7 @@ df.columns = ['Label','Text']
 # Convert class to a Label (Factor in R)
 df['Label'] = pd.Categorical(df['Label'])
 df['Label'] = df['Label'].astype("category")
-
+categories = pd.Categorical(df['Label']).categories
 # Missing Values
 df.info()
 
@@ -43,7 +46,29 @@ df['Length'] = df['Text'].apply(lambda x: len(x))
 df['Length'].describe()
 
 # Distribution of Lenght of Texts based on their labels
-df.groupby('Label').hist(sharex=True, sharey=False, ax=plt.gca())
+fig = plt.figure()
+ax = plt.subplot(111)
+for cat in categories:
+    ax.hist(df.loc[df['Label'] == cat, 'Length'],bins=50)
+plt.show()
+
+
+# Prepare Data for Modeling 
+# ---------------------------------------------------
+
+X = np.arange(10)
+y = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+sampler1 = StratifiedKFold(n_splits=3)
+sampler2 = StratifiedShuffleSplit(n_splits=3,test_size=0.4)
+
+print('Stratified-K-Fold')
+for t_id, v_id in sampler1.split(X=X,y=y):
+    print(t_id,v_id)
+
+print('Stratified-Shuffle-Split')
+for t_id, v_id in sampler2.split(X=X,y=y):
+    print(t_id,v_id)
 
 
 
